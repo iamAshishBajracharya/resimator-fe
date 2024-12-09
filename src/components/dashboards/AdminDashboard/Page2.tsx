fbafeb3d54c573db4ecc292ca764278490eaad50
Ashish Bajracharya <ashish.bajracharya@cnspartner.com>
2024-12-09 06:49:40 +0545

    adds swtich component

--- src/components/dashboards/AdminDashboard/Page2.tsx
+++ src/components/dashboards/AdminDashboard/Page2.tsx
@@ -1,8 +1,8 @@
-import { Breadcrumb, Typography } from "antd";
+import { Breadcrumb, Switch, Typography } from "antd";
 import UserTable from "../widgets/UserTable";
 import homeIcon from '../../../assets/icon-home.svg'
 import styled from "styled-components";
-import {RightOutlined} from '@ant-design/icons'
+import { RightOutlined } from '@ant-design/icons'
 import { Link } from "react-router-dom";
 const StyledIconContainer = styled.div`
 width: 14px;
@@ -10,6 +10,8 @@ width: 14px;
 
 const StyledTitleWrapper = styled.div`
 margin-bottom: 12px;
+display:flex;
+justify-content: space-between;
 h2{
 font-weight: 600px;
 color: #191A1A;
@@ -20,42 +22,46 @@ const StyledBreadCrumbsWrapper = styled.div`
 margin-bottom: 16px;
 `
 
-const {Title} = Typography
+const { Title } = Typography
 const Page2 = () => {
     return (
         <>
             <StyledTitleWrapper>
                 <Title level={2}>User Overview</Title>
+                <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                    <Switch defaultChecked />
+                    <span>All</span>
+                </div>
             </StyledTitleWrapper>
             <StyledBreadCrumbsWrapper>
-            <Breadcrumb
-                items={[
-                    {
-                        title: (
-                            <Link to = "/">
-                                <StyledIconContainer>
-                                <img src={homeIcon} alt="" />
-                            </StyledIconContainer>
-                            </Link>
-                            
-                        ),
-                    },
-                    {
-                        title: (
-                            <Link to = "/">
-                            Settings
-                            </Link>
-                            
-                        ),
-                    },
-                    {
-                        title: 'User',
-                    },
-                ]}
-                separator={<RightOutlined />}
-            />
+                <Breadcrumb
+                    items={[
+                        {
+                            title: (
+                                <Link to="/">
+                                    <StyledIconContainer>
+                                        <img src={homeIcon} alt="" />
+                                    </StyledIconContainer>
+                                </Link>
+
+                            ),
+                        },
+                        {
+                            title: (
+                                <Link to="/">
+                                    Settings
+                                </Link>
+
+                            ),
+                        },
+                        {
+                            title: 'User',
+                        },
+                    ]}
+                    separator={<RightOutlined />}
+                />
             </StyledBreadCrumbsWrapper>
-            
+
             <UserTable />
         </>
 
